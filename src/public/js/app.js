3de817218d5a031ce50119eb347b8028b39c2516
soyeon <luora@naver.com>
2022-11-19 05:22:27 +0900

    [add] WebSocket events [#1]

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,18 @@
-alert("hi");
+// frontend에서 backend로 연결
+const socket = new WebSocket(`ws://${window.location.host}`)
+
+socket.addEventListener("open", () => {
+    console.log("Connected to Server 🐣");
+})
+
+socket.addEventListener("message", (message) => {
+    console.log("New message : " , message.data);
+})
+
+socket.addEventListener("close", () => {
+    console.log("Disconnected from Server ❌");
+})
+
+setTimeout( () => {
+    socket.send("hello from the browser");
+}, 10000);
--- src/server.js
+++ src/server.js
@@ -1,12 +1,27 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
 
 app.set("view engine", "pug");
-app.set("views", __dirname + "/views")
-app.use("/public", express.static(__dirname + "/public"))
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
 
-app.get("/", (req, res) => res.render("home"))
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+//app.listen(3003, handleListen);
 
-const handleListen = () => console.log(`Listening on http://localhost:3003`)
-app.listen(3003, handleListen);
+const server = http.createServer(app); // http 서버
+const wss = new WebSocket.Server({server}); // websocket 서버
+
+wss.on("connection", (socket) => {
+    console.log("Connected to Browser 🐣");
+    socket.on("close", () => console.log("Disconnected from the Browser  ❌"))
+    socket.on("message", message => {
+        console.log(message.toString());
+    })
+    socket.send("hello");
+})
+server.listen(3000, handleListen);
